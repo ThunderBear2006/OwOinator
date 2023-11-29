a9e412b86af982406be667a54abd2f797e9ab99d
fikfr <fikfr@seznam.cz>
2023-11-29 07:49:54 +0100

    modified:   Form1.cs 	modified:   Form1.Designer.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,65 +8,89 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+namespace FiksHomeWork
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer moveTimer;
+        private int moveSpeed = 5; // Adjust the move speed as needed
 
-namespace FiksHomeWork;
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (ModifierKeys == Keys.None && keyData == Keys.U)
+            {
+                System.Environment.Exit(1);
+            }
+            return base.ProcessDialogKey(keyData);
+        }
 
-public partial class Form1 : Form
-	{
-		protected override bool ProcessDialogKey(Keys keyData)
-		{
-			if (ModifierKeys == Keys.None && keyData == Keys.U)
-			{
-			    System.Environment.Exit(1);
-			}
-			return base.ProcessDialogKey(keyData);
-		}
+        public Form1()
+        {
+            InitializeComponent();
+            Program.Heads.Append(this);
 
-		public Form1()
-		{
-			InitializeComponent();
-			Program.Heads.Append(this);
-		}
+            // Initialize and start the timer for form movement
+            moveTimer = new Timer();
+            moveTimer.Interval = 20; // Adjust the interval as needed
+            moveTimer.Tick += MoveTimer_Tick;
+            moveTimer.Start();
+        }
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			TopMost=true;
-			ShowInTaskbar = false;
-		}
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            // Move the form horizontally
+            this.Left += moveSpeed;
 
-		public void Tick() {
-			if (Program.HeadsCut > 5 && Program.HeadsCut < 7) {
-				SetDesktopLocation(Location.X + Program.random.Next(-10, 10), Location.Y + Program.random.Next(-10, 10));
-			}
-		}
+            // Check if the form has reached the screen bounds
+            if (this.Right >= Screen.PrimaryScreen.Bounds.Right || this.Left <= Screen.PrimaryScreen.Bounds.Left)
+            {
+                // Reverse the horizontal direction
+                moveSpeed = -moveSpeed;
+            }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-            for (int i = 0; i <= Program.HeadsCut; i++) {
+            // Move the form vertically
+            this.Top += moveSpeed;
+
+            // Check if the form has reached the screen bounds
+            if (this.Bottom >= Screen.PrimaryScreen.Bounds.Bottom || this.Top <= Screen.PrimaryScreen.Bounds.Top)
+            {
+                // Reverse the vertical direction
+                moveSpeed = -moveSpeed;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TopMost = true;
+            ShowInTaskbar = false;
+        }
+
+        public void Tick()
+        {
+            if (Program.HeadsCut > 5 && Program.HeadsCut < 7)
+            {
+                SetDesktopLocation(Location.X + Program.random.Next(-10, 10), Location.Y + Program.random.Next(-10, 10));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= Program.HeadsCut; i++)
+            {
                 Form newForm = new Form1();
-				Program.Heads.Append(newForm);
+                Program.Heads.Append(newForm);
                 newForm.Show();
             }
 
             Program.HeadsCut++;
 
             Hide();
-		}
+        }
 
-		private void label1_Click(object sender, EventArgs e)
-		{
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-		}
-
-		
-
-		private void button1_Click_1(object sender, EventArgs e)
-		{
-			
-		}
-
-      
-
-		
-
-	}
+        }
+    }
+}
